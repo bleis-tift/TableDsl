c1799394e47c122bee1b14395dec56e099135280
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-09-12 13:17:15 +0900

    Printerを実装開始

---
+++ TableDsl.Core.Tests/PrinterTest.fs
@@ -0,0 +1,21 @@
+﻿namespace TableDsl.Core.Tests
+
+open FsUnit
+open NUnit.Framework
+
+[<TestFixture>]
+module PrinterTest =
+  open TableDsl
+
+  [<Test>]
+  let ``print empty list`` () =
+    []
+    |> Printer.print
+    |> should equal ""
+
+  [<TestCase("coltype Created = datetime2")>]
+  let tests str =
+    str
+    |> Parser.parse
+    |> Printer.print
+    |> should equal str
---
+++ TableDsl.Core/Printer.fs
@@ -0,0 +1,51 @@
+﻿namespace TableDsl
+
+open Basis.Core
+
+module Printer =
+  let printSummary indent = function
+  | Some summary ->
+      summary
+      |> Str.splitBy "\n"
+      |> Array.map (fun line -> (String.replicate indent " ") + "/// " + line)
+      |> Str.join "\n"
+  | None -> ""
+
+  let printJpName = function
+  | Some jpName -> "[" + jpName + "]"
+  | None -> ""
+
+  let printOpenTypeParam = function
+  | TypeVariable v -> v
+  | BoundValue v -> v
+
+  let printOpenTypeParams = function
+  | [] -> ""
+  | typeVars -> "(" + (typeVars |> List.map printOpenTypeParam |> Str.join ", ") + ")"
+
+  let printColumnTypeDef col =
+    " " +
+      match col.ColumnTypeDef with
+      | BuiltinType typ -> typ.TypeName
+      | AliasDef (typ, originalType) -> failwith "Not implemented yet"
+      | EnumTypeDef typ -> failwith "Not implemented yet"
+
+  let printTypeDef = function
+  | BuiltinType _ -> failwith "組み込み型をcoltypeとして出力することはできません。"
+  | AliasDef (typ, originalType) -> (typ.TypeName + (printOpenTypeParams typ.TypeParameters), printColumnTypeDef originalType)
+  | EnumTypeDef typ -> failwith "Not implemented yet"
+
+  let printTableDef table = ""
+
+  let printColTypeDef (col: ColumnTypeDef) =
+    let summary = printSummary 0 col.ColumnSummary
+    let jpName = printJpName col.ColumnJpName
+    let name, body = printTypeDef col.ColumnTypeDef
+    sprintf "%scoltype %s%s =%s" summary name jpName body
+
+  let printElem = function
+  | TableDef table -> printTableDef table
+  | ColTypeDef col -> printColTypeDef col
+
+  let print (elements: Element list) =
+    elements |> List.map printElem |> Str.join "\n"
