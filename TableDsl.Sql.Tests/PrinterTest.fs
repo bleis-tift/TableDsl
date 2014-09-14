167bd6b9f561b4ae32ac8183f27dd28d9898d21b
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-09-14 14:48:08 +0900

    単純なCREATE TABLEの出力に対応

--- TableDsl.Sql.Tests/PrinterTest.fs
+++ TableDsl.Sql.Tests/PrinterTest.fs
@@ -5,6 +5,7 @@ open NUnit.Framework
 
 [<TestFixture>]
 module PrinterTest =
+  open TableDsl
   open TableDsl.Sql
   open Basis.Core
 
@@ -13,3 +14,44 @@ module PrinterTest =
     []
     |> Printer.print
     |> should equal ""
+
+  let trimAndCountIndent (str: string) =
+    let indent = str |> Seq.takeWhile ((=)' ') |> Seq.length
+    (str |> Str.subFrom indent, indent)
+
+  let adjust str =
+    let lines =
+      str |> Str.replace "\r\n" "\n" |> Str.splitBy "\n" |> Array.toList
+    let adjusted =
+      match lines with
+      | [] -> []
+      | [line] -> [line]
+      | ""::first::rest ->
+          let first, indent = trimAndCountIndent first
+          first::(rest |> List.map (Str.subFrom indent))
+      | _ ->
+          failwithf "oops! %A" lines
+    adjusted |> Str.join "\n"
+
+  let source =
+    [
+      """
+      table Users = {
+        Id: int
+        Name: nvarchar(16)
+      }""", """
+      CREATE TABLE [Users] (
+          [Id] int NOT NULL
+        , [Name] nvarchar(16) NOT NULL
+      );"""
+    ]
+    |> List.map (fun (a, b) -> (adjust a, adjust b))
+
+  [<TestCaseSource("source")>]
+  let tests (args: System.Tuple<string, string>) =
+    let input = args.Item1
+    let expected = args.Item2
+    input
+    |> Parser.parse
+    |> Printer.print
+    |> should equal expected
--- TableDsl.Sql/Printer.fs
+++ TableDsl.Sql/Printer.fs
@@ -1,7 +1,59 @@
 ﻿namespace TableDsl.Sql
 
+open Basis.Core
 open TableDsl
 
 module Printer =
-  let print (elems: Element list) =
+  let columnTypeName colTyp =
+    match colTyp with
+    | BuiltinType typ
+    | AliasDef (typ, _) -> typ.TypeName
+    | EnumTypeDef typ -> typ.EnumTypeName
+
+  let printColumnTypeName (typ: ClosedTypeRef) =
+    let typeName = columnTypeName typ.Type.ColumnTypeDef
+    let typeParams =
+      match typ.TypeParameters with
+      | [] -> ""
+      | notEmpty -> "(" + (notEmpty |> Str.join ", ") + ")"
+    typeName + typeParams + " NOT NULL"
+
+  let printColumnDef col =
+    let typ, _ = col.ColumnType
+    let name =
+      match col.ColumnName with
+      | ColumnName (name, _) -> name
+      | Wildcard -> columnTypeName typ.Type.ColumnTypeDef
+    "[" + name + "] " + (printColumnTypeName typ)
+
+  let printCreateTable table =
+    "CREATE TABLE [" + table.TableName + "] (\n"
+      + "    " + (table.ColumnDefs |> List.map printColumnDef |> Str.join "\n  , ")
+      + "\n);"
+
+  let printAlterTable table =
     ""
+
+  let printSummaryAndJpName table =
+    ""
+
+  let print elems =
+    let targets =
+      elems |> List.choose (function TableDef t -> Some t | _ -> None)
+
+    let createTable =
+      targets
+      |> List.map printCreateTable
+      |> Str.join "\n"
+    let alterTable =
+      targets
+      |> List.map printAlterTable
+      |> Str.join "\n"
+    let summaryAndJpName =
+      targets
+      |> List.map printSummaryAndJpName
+      |> Str.join "\n"
+
+    createTable
+      + (if alterTable <> "" then "\n" + alterTable else "")
+      + (if summaryAndJpName <> "" then "\n" + summaryAndJpName else "")
