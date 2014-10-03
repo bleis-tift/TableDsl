40bef720b16ea5ea3f3ae62e9c239982d74df938
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-10-03 16:59:55 +0900

    SQLのPrinterのテストを見やすくした

--- TableDsl.Sql.Tests/PrinterTest.fs
+++ TableDsl.Sql.Tests/PrinterTest.fs
@@ -33,77 +33,85 @@ module PrinterTest =
           failwithf "oops! %A" lines
     adjusted |> Str.join "\n"
 
+  type Source = {
+    Input: string
+    Expected: string
+  }
+
   let source =
     [
-      """
-      table Users = {
-        Id: int
-        Name: nvarchar(16)
-      }""", """
-      CREATE TABLE [Users] (
-          [Id] int NOT NULL
-        , [Name] nvarchar(16) NOT NULL
-      );"""
-      """
-      table Users = {
-        Id: int
-        Name: nvarchar(16)
-      }
-      table DeletedUsers = {
-        Id: int
-        UserId: int
-      }""", """
-      CREATE TABLE [Users] (
-          [Id] int NOT NULL
-        , [Name] nvarchar(16) NOT NULL
-      );
-      CREATE TABLE [DeletedUsers] (
-          [Id] int NOT NULL
-        , [UserId] int NOT NULL
-      );"""
-      """
-      table Users = {
-        Id: { uniqueidentifier with PK }
-      }""", """
-      CREATE TABLE [Users] (
-          [Id] uniqueidentifier NOT NULL
-      );
-      ALTER TABLE [Users] ADD CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED (
-          [Id]
-      );"""
-      """
-      table Users = {
-        Name: { nvarchar(128) with PK = PK1 }
-        Age: { int with PK = PK1 }
-      }""", """
-      CREATE TABLE [Users] (
-          [Name] nvarchar(128) NOT NULL
-        , [Age] int NOT NULL
-      );
-      ALTER TABLE [Users] ADD CONSTRAINT [PK1_Users] PRIMARY KEY CLUSTERED (
-          [Name]
-        , [Age]
-      );"""
-      """
-      table Users = {
-        Name: { nvarchar(128) with PK = PK1.2 }
-        Age: { int with PK = PK1.1 }
-      }""", """
-      CREATE TABLE [Users] (
-          [Name] nvarchar(128) NOT NULL
-        , [Age] int NOT NULL
-      );
-      ALTER TABLE [Users] ADD CONSTRAINT [PK1_Users] PRIMARY KEY CLUSTERED (
-          [Age]
-        , [Name]
-      );"""
+      { Input = """
+                table Users = {
+                  Id: int
+                  Name: nvarchar(16)
+                }"""
+        Expected = """
+                   CREATE TABLE [Users] (
+                       [Id] int NOT NULL
+                     , [Name] nvarchar(16) NOT NULL
+                   );""" }
+      { Input = """
+                table Users = {
+                  Id: int
+                  Name: nvarchar(16)
+                }
+                table DeletedUsers = {
+                  Id: int
+                  UserId: int
+                }"""
+        Expected = """
+                   CREATE TABLE [Users] (
+                       [Id] int NOT NULL
+                     , [Name] nvarchar(16) NOT NULL
+                   );
+                   CREATE TABLE [DeletedUsers] (
+                       [Id] int NOT NULL
+                     , [UserId] int NOT NULL
+                   );""" }
+      { Input = """
+                table Users = {
+                  Id: { uniqueidentifier with PK }
+                }"""
+        Expected = """
+                   CREATE TABLE [Users] (
+                       [Id] uniqueidentifier NOT NULL
+                   );
+                   ALTER TABLE [Users] ADD CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED (
+                       [Id]
+                   );""" }
+      { Input = """
+                table Users = {
+                  Name: { nvarchar(128) with PK = PK1 }
+                  Age: { int with PK = PK1 }
+                }"""
+        Expected = """
+                   CREATE TABLE [Users] (
+                       [Name] nvarchar(128) NOT NULL
+                     , [Age] int NOT NULL
+                   );
+                   ALTER TABLE [Users] ADD CONSTRAINT [PK1_Users] PRIMARY KEY CLUSTERED (
+                       [Name]
+                     , [Age]
+                   );""" }
+      { Input = """
+                table Users = {
+                  Name: { nvarchar(128) with PK = PK1.2 }
+                  Age: { int with PK = PK1.1 }
+                }"""
+        Expected = """
+                   CREATE TABLE [Users] (
+                       [Name] nvarchar(128) NOT NULL
+                     , [Age] int NOT NULL
+                   );
+                   ALTER TABLE [Users] ADD CONSTRAINT [PK1_Users] PRIMARY KEY CLUSTERED (
+                       [Age]
+                     , [Name]
+                   );""" }
     ]
-    |> List.map (fun (a, b) -> (adjust a, adjust b))
+    |> List.map (fun { Input = a; Expected = b} -> { Input = adjust a; Expected = adjust b })
 
   [<TestCaseSource("source")>]
-  let tests (args: System.Tuple<string, string>) =
-    let input = args.Item1
-    let expected = args.Item2
+  let tests { Input = input; Expected = expected } =
     input
     |> Parser.parse
     |> Printer.print
