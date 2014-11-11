50f5a9ae997e2b7d09794459f87a720abdb7d186
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-11 16:27:22 +0900

    パッケージ生成用スクリプトを追加

---
+++ New-Packages.ps1
@@ -0,0 +1,15 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe TableDsl.sln /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
+
+cd TableDsl.Core
+..\.nuget\nuget.exe pack TableDsl.Core.fsproj -Build -Symbols -Properties VisualStudioVersion=12.0
+cd ..\TableDsl.Sql
+..\.nuget\nuget.exe pack TableDsl.Sql.fsproj -Build -Symbols -Properties VisualStudioVersion=12.0
+cd ..\TableDsl
+..\.nuget\nuget.exe pack TableDsl.fsproj -Properties VisualStudioVersion=12.0
+
+cd ..
+
+mkdir nuget-packages
+mv TableDsl.Core\*.nupkg nuget-packages
+mv TableDsl.Sql\*.nupkg nuget-packages
+mv TableDsl\*.nupkg nuget-packages
