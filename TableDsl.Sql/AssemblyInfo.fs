b9d658b39ae1b7027ac775e44f5b70e656bf8782
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-11-11 15:40:59 +0900

    AssemblyInfoを追加

---
+++ TableDsl.Sql/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace global
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("TableDsl.Sql")>]
+[<assembly: AssemblyProduct("TableDsl.Sql")>]
+[<assembly: Guid("6D114E8C-6FA3-42FD-BB90-39DA71E3495D")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do ()
---
+++ TableDsl/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace global
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("TableDsl")>]
+[<assembly: AssemblyProduct("TableDsl")>]
+[<assembly: Guid("09E28C01-A9CD-4EA9-8512-987DDA9F8980")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do ()
